4d799cf7bb5f1e40542ce2ce84602aabd7b9482b
NiceyLai <laisi1127@163.com>
2022-08-29 21:12:40 +0800

    update

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,13 +1,17 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div>
-statistics.vue
+        statistics.vue
+        <Nav/>
     </div>
 </template>
 
 <script lang="ts">
+     import Nav from '@/components/Nav.vue'
     export default {
-     
+        // eslint-disable-next-line vue/multi-word-component-names
+        name:'Money',
+     components:{Nav}
     };
 </script>
 
