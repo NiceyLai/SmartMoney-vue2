c6e134aeb73e3b577382ad69cd3a313783340908
nicey <1919325005@qq.com>
2022-08-29 16:45:38 +0800

    创创建三个页面

---
+++ src/views/Statistics.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+statistics.vue
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+     
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
