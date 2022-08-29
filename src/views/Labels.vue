512802ae19e157bbdd4fb6cd8a87bbdb22484c56
NiceyLai <laisi1127@163.com>
2022-08-29 16:45:38 +0800

    创创建三个页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+label.vue
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        // eslint-disable-next-line vue/multi-word-component-names
+        name:'Labels'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+Money.vue
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+     
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
