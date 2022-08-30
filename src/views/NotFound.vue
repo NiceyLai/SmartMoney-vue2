eb9b7066b55c768ebde18d1351be693fb3c6bf43
nicey <1919325005@qq.com>
2022-08-30 10:03:36 +0800

    404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <div>当前页面不存在，请检查网址是否正确</div>
+        <div>
+            <!-- 第一种写法 -->
+            <router-link to="/">返回首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+          // eslint-disable-next-line vue/multi-word-component-names
+        name:'NotFound',
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
