ca153e3af575bc7b9c4c52b5fb677f5554c4ca3c
nicey <1919325005@qq.com>
2022-08-30 16:13:12 +0800

    添加 Nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,36 +1,49 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="nav">
-      <router-link to="/money">
-        <Icon name="money"/>
-        记账
-        </router-link>
-      |
-      <router-link to="/labels">
-        <Icon name="label"/>
-        标签
-        </router-link>
-      |
-      <router-link to="/statistics">
-        <Icon name="statistics"/>
-        统计
-        </router-link>
-    </div>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
 
-import Icon from "./Icon.vue";
-
     export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: "Nav",
-    components: { Icon }
 }
 </script>
 
 <style lang="scss" scoped>
-.nav{
-    border:1px solid red
-}
+ nav {
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+    > .item {
+      padding: 2px 0;
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    > .item.selected{
+       color: #42b983;
+    }
+  }
 </style>
