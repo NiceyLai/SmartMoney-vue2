6111105e859f02638aa95a9d88ddedb7e7c91e88
nicey <laisi1127@163.com>
2022-08-31 16:48:13 +0800

    解决了SVG自带颜色的问题

--- vue.config.js
+++ vue.config.js
@@ -14,9 +14,26 @@ module.exports = defineConfig({
       .use("svg-sprite-loader" /*使用svg-sprite-loader*/)
       .loader("svg-sprite-loader")
       .options({ extract: false } /*不要解析出文件*/)
+      .end()
+      .use("svgo-loader")
+      .loader("svgo-loader")
+      .tap((options) => ({
+        ...options,
+        plugins: [{ removeAttrs: { attrs: "fill" } }],
+      }))
       .end();
     config.plugin("svg-sprite").use(require("svg-sprite-loader/plugin")),
       [{ pluginSprite: true }]; //配置插件
     config.module.rule("svg").exclude.add(dir); //其他svg loader排除 icons目录
   },
+  // config.module
+  //   .rule('svg-sprite')
+  //   .test(/\.(svg)(\?.*)?$/)
+  //   .include.add(dir).end()
+  //   .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
+  //   .use('svgo-loader').loader('svgo-loader')
+  //   .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]}))
+  //   .end()
+  // config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
+  // config.module.rule('svg').exclude.add(dir)
 });
