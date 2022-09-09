3c798f3be51f596846b0498f125cf03c31ce44c0
nicey <laisi1127@163.com>
2022-09-09 16:04:34 +0800

    收集四个组件的数据到record

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <Layout class-prefix="layout">
+    {{ record }}
     <Number @update:value="onUpdateAmount"></Number>
-    <Types @update:value="onUpdateType"></Types>
+    <Types :value.sync="record.type"></Types>
     <Notes @update:value="onUpdateNotes"></Notes>
-    <Tages :data-source.sync="tages" @update:value="onUpdateTages"></Tages>
+    <tages :data-source.sync="tages" @update:value="onUpdateTages"></tages>
   </Layout>
 </template>
 
@@ -26,21 +27,18 @@ type Record = {
 @Component({ components: { Tages, Notes, Types, Number } })
 export default class Money extends Vue {
   tages = ["衣", "食", "住", "行", "买"];
+  record: Record = { tages: [], notes: "", type: "-", amount: 0 };
 
   onUpdateTages(value: string[]) {
-    console.log(value);
+    this.record.tages = value;
   }
 
-  onUpdateNotes(value: string[]) {
-    console.log(value);
-  }
-
-  onUpdateType(value: string) {
-    console.log(value);
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
   }
 
   onUpdateAmount(value: string) {
-    console.log(value);
+    this.record.amount = parseFloat(value);
   }
 }
 </script>
