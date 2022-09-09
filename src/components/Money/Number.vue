86106e41360b0f6d0962738941a36d6281601569
nicey <laisi1127@163.com>
2022-09-09 16:49:38 +0800

    完善numberPad中OK键问题

--- src/components/Money/Number.vue
+++ src/components/Money/Number.vue
@@ -63,6 +63,8 @@ export default class NumberPad extends Vue {
   ok() {
     this.$emit("update:value", this.output);
     this.$emit("submit", this.output);
+    this.output = "0";
+
     console.log("提交记账");
   }
 }
