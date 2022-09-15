029740a781aecdbfc6210b52eb4f7f3b03b71e88
nicey <laisi1127@163.com>
2022-09-15 18:11:16 +0800

    模块化store，删除了models

--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -1,25 +1,64 @@
-import tagListModel from "@/models/tagListModel";
+import createId from "@/lib/createId";
 
-export default {
-    //  tag store
-  tagList: tagListModel.fetch(),
-  findTag (id: string){
+const localStorageKeyName = "tagList";
+
+const tagStore = {
+  tagList: [] as Tag[],
+  fetchTags() {
+    this.tagList= JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]");
+    return this.tagList;
+  },
+  findTag(id: string) {
     return this.tagList.filter((t) => t.id === id)[0];
   },
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === "duplicated") {
+  createTag(name: string) {
+    const names = this.tagList.map((item) => item.name);
+    if (names.indexOf(name) >= 0) {
       window.alert("标签名重复了，请重新操作");
-    } else if (message === "success") {
-      window.alert("添加标签名成功！");
+      return "duplicated";
+    }
+    const id = createId().toString();
+    this.tagList.push({ id, name: name });
+    this.saveTags();
+    window.alert("添加标签名成功！");
+    return "success";
+  },
+  removeTag(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.tagList.length; i++) {
+      if (this.tagList[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.tagList.splice(index, 1);
+    this.saveTags();
+    return true;
+  },
+  updateTag(id: string, name: string) {
+    const idList = this.tagList.map((item) => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.tagList.map((item) => item.name);
+      if (names.indexOf(name) >= 0) {
+        return "duplicated";
+      } else {
+        const tag = this.tagList.filter((item) => item.id === id)[0];
+        tag.name = name;
+        this.saveTags();
+        return "success";
+      }
+    } else {
+      return "not found";
     }
   },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
+  saveTags() {
+    window.localStorage.setItem(
+      localStorageKeyName,
+      JSON.stringify(this.tagList)
+    );
   },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
+};
 
+tagStore.fetchTags();
 
-}
+export default tagStore;
