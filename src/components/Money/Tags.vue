0d0e9cfd69d9d7c3acef06dad69009a56fdc8ab3
nicey <laisi1127@163.com>
2022-09-16 14:55:11 +0800

    tags.vue 使用 vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -24,15 +24,17 @@ import { Component } from "vue-property-decorator";
 @Component({
   computed: {
     tagList() {
-      // todo
-
-      // return  this.$store.fetchTags();
-      return [];
+      return this.$store.state.tagList;
     },
   },
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit("fetchTags");
+  }
+
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
@@ -47,8 +49,7 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert("标签名不能为空");
     }
-    // todo
-    // store.createTag(name);
+    this.$store.commit("createTag", name);
   }
 }
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import clone from "@/lib/clone";
+import createId from "@/lib/createId";
 import Vue from "vue";
 import Vuex from "vuex";
 
@@ -7,9 +8,8 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     recordList: [] as RecordItem[],
+    tagList: [] as Tag[],
   },
-
-  getters: {},
 
   mutations: {
     fetchRecords(state) {
@@ -29,11 +29,27 @@ const store = new Vuex.Store({
         JSON.stringify(state.recordList)
       );
     },
+    fetchTags(state) {
+      return (state.tagList = JSON.parse(
+        window.localStorage.getItem("tagList") || "[]"
+      ));
+    },
+    createTag(state, name: string) {
+      const names = state.tagList.map((item) => item.name);
+      if (names.indexOf(name) >= 0) {
+        window.alert("标签名重复了，请重新操作");
+        return "duplicated";
+      }
+      const id = createId().toString();
+      state.tagList.push({ id, name: name });
+      store.commit("saveTags");
+      window.alert("添加标签名成功！");
+      return "success";
+    },
+    saveTags(state) {
+      window.localStorage.setItem("tagList", JSON.stringify(state.tagList));
+    },
   },
-
-  actions: {},
-
-  modules: {},
 });
 
 export default store;
