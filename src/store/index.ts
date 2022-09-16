ce0ed4db29fa6617d897910a64e85324fcf689b4
nicey <laisi1127@163.com>
2022-09-16 21:23:28 +0800

    修复刷新变成404的bugug

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,16 @@
 import clone from "@/lib/clone";
 import createId from "@/lib/createId";
+import router from "@/router";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 type RootsState = {
-      recordList: RecordItem[],
-    tagList:Tag[],
-    currentTag?:Tag
-}
+  recordList: RecordItem[];
+  tagList: Tag[];
+  currentTag?: Tag;
+};
 
 const store = new Vuex.Store({
   state: {
@@ -19,9 +20,40 @@ const store = new Vuex.Store({
   } as RootsState,
 
   mutations: {
-    setCurrentTag(state,id: string) {
-     state.currentTag = state.tagList.filter((t) => t.id === id)[0];
+    setCurrentTag(state, id: string) {
+      state.currentTag = state.tagList.filter((t) => t.id === id)[0];
     },
+    updateTag(state, payload: { id: string; name: string }) {
+      const { id, name } = payload;
+      const idList = state.tagList.map((item) => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = state.tagList.map((item) => item.name);
+        if (names.indexOf(name) >= 0) {
+          window.alert("标签名重复了，请重新操作");
+        } else {
+          const tag = state.tagList.filter((item) => item.id === id)[0];
+          tag.name = name;
+          store.commit("saveRecords");
+        }
+      }
+    },
+    removeTag(state, id: string) {
+      let index = -1;
+      for (let i = 0; i < state.tagList.length; i++) {
+        if (state.tagList[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0) {
+        state.tagList.splice(index, 1);
+        store.commit("saveTags");
+        router.back();
+      } else {
+        window.alert("删除失败！");
+      }
+    },
+
     fetchRecords(state) {
       state.recordList = JSON.parse(
         window.localStorage.getItem("recordList") || "[]"
@@ -57,7 +89,6 @@ const store = new Vuex.Store({
     saveTags(state) {
       window.localStorage.setItem("tagList", JSON.stringify(state.tagList));
     },
-
   },
 });
 
